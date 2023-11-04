fd352f8131ff709714b8beef5c1cb41445347177
CihideungHilir <148986406+CihideungHilir@users.noreply.github.com>
2023-11-04 23:36:59 +0700

    Update MapView.vue
    Peta tapal batas

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -101,7 +101,7 @@ export default {
     async loadSomeGeoJson() {
       const nextIndex = this.layerData.length;
 
-      const response = await fetch("/ds_nyule.geojson");
+      const response = await fetch("/TAPAL_BATAS_FeaturesToJSON.json");
       const data = await response.json();
 
       console.log(data);
